49db495fe15be27bbfe88ea624dd621914a33505
Storrm113 <darinburden@gmail.com>
2024-12-12 08:34:40 -0500

    block07

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ add alert box with message and reveled pin*/
 const pin1 = 3 + 7;
 const pin2 = 80 / 2;
 const pin3 = 3 * 13;
-let message =
-  "You have received this message becuase you have been chosen to open an important vault. Here lies the seceret combination:";
+/*a above are the strings for the combination for the vault*/
 alert(
   `You have received this message becuase you have been chosen to open an important vault. Here lies the seceret combination: ${pin1} ${pin2} ${pin3}`
 );
+/* above is the alert box for the combination for the vault with a greating.*/
