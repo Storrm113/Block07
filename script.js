716272f96f7b5ae4ab01b67393e81046162dbe17
Storrm113 <darinburden@gmail.com>
2024-12-12 08:27:34 -0500

    block07

---
+++ script.js
@@ -0,0 +1,11 @@
+/*psuedo first step is to set values to ids for pinlock,
+setup script for the values
+add alert box with message and reveled pin*/
+const pin1 = 3 + 7;
+const pin2 = 80 / 2;
+const pin3 = 3 * 13;
+let message =
+  "You have received this message becuase you have been chosen to open an important vault. Here lies the seceret combination:";
+alert(
+  `You have received this message becuase you have been chosen to open an important vault. Here lies the seceret combination: ${pin1} ${pin2} ${pin3}`
+);
